a8921eb22ff67d2f7f0231030d3de66bd07e49ea
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2025-01-13 20:15:02 +0530

    Update HIndexLeetCodes.py

--- HIndexLeetCodes.py
+++ HIndexLeetCodes.py
@@ -12,12 +12,8 @@ Output: 1
 */
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        n = len(citations)
-        left, right = 0, n
-        while left < right:
-            mid = (left + right + 1) >> 1
-            if citations[n - mid] >= mid:
-                left = mid
-            else:
-                right = mid - 1
-        return left
+       citations.sort(reverse=True)
+        for h in range(len(citations), 0, -1):
+            if citations[h - 1] >= h:
+                return h
+        return 0
