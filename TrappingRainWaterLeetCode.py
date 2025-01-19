47aadd9f982214b819b87d99b004a7ded1508b3d
MD FAISHAL ANIS <137764041+FaishalAnis@users.noreply.github.com>
2025-01-19 12:12:23 +0530

    Create TrappingRainWaterLeetCode.py

---
+++ TrappingRainWaterLeetCode.py
@@ -0,0 +1,27 @@
+/*42. Trapping Rain Water
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9
+
+Constraints:
+n == height.length
+1 <= n <= 2 * 104
+0 <= height[i] <= 105
+*/
+class Solution:
+    def trap(self, heights: List[int]) -> int:
+        n = len(heights)
+        l, r = [0] * (n + 1), [0] * (n + 1)
+        ans = 0
+        for i in range(1, len(heights) + 1):
+            l[i] = max(l[i - 1], heights[i - 1])
+        for i in range(len(heights) - 1, 0, -1):
+            r[i] = max(r[i + 1], heights[i])
+        for i in range(len(heights)):
+            ans += max(0, min(l[i + 1], r[i]) - heights[i])
+        return ans
